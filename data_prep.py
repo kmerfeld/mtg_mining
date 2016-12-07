2e162e7f0d0cd581d3409161e9e022adefe5661d
kyle merfeld <kmerfeld1@gmail.com>
2016-12-07 17:55:42 -0600

    shoving stuff

--- data_prep.py
+++ data_prep.py
@@ -1,28 +1,48 @@
 #!/usr/bin/env python
-
+# -*- coding: utf-8 -*-
 import json
 from pprint import pprint
 import codecs
 import sys
 from get_ability import *
-#create db
+import re
 from mtgjson import CardDb
+
+
+#create db
 db = CardDb.from_file(db_file='AllSets.json')
 
 
 #read in our list of cards to check
-with open('list_of_cards') as f:
-    lines = f.read().splitlines()
-
-
+with open('ORI.json', encoding="utf-8") as f:
+    lines = f.read()
+    print(type(lines))
+    lines = json.loads(lines)
+    #x = str(lines).encode("utf-8")
+#print(x)
+#print(str(lines['cards']).encode('utf-8'))
 #getlist of cards
 card_list = []
-for item in lines:
-    x = db.cards_by_name[item]
-    card_list.append(x)
+
+
+for key in lines['cards']:
+    if "<class 'str'>" in key['name']:
+        continue
+    name =key['name'].encode('utf-8').decode('UTF-8')
+    card = db.cards_by_name[name]
+
+
+    if "Creature" == card.types[0]:
+        if len(card.colors) == 1:
+            card_list.append(card)
+
+
+    
+ 
 
 #get the values we want from the list of cards
 for item in card_list:
+    print("##",item.name)
     if item.power == "*":
         adjusted_power = "*"
     elif item.power == "0":
@@ -45,10 +65,14 @@ for item in card_list:
 
     else:
         adjusted_toughness = int(item.toughness)/int(item.cmc)
-
+    
+    tf = ""
+    if len(item.subtypes) == 2:
+        tf = item.subtypes[1] 
 
     ability = get_ability(item)  
-    print(str(adjusted_power) + "," + str(adjusted_toughness) + "," + item.subtypes[0] + "," + ability + "," + item.colors[0])
+    
+    print(str(item.cmc) + "," + str(adjusted_power) + "," + str(adjusted_toughness) + "," + item.subtypes[0] + "," + tf  + "," + ability + "," + item.colors[0])
 
 
 
---
+++ data_prep_Full_set.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import json
+from pprint import pprint
+import codecs
+import sys
+from get_ability import *
+import re
+from mtgjson import CardDb
+
+
+#create db
+db = CardDb.from_file(db_file='AllSets.json')
+
+
+#read in our list of cards to check
+with open('AllSets.json', encoding="utf-8") as f:
+    lines = f.read()
+    print(type(lines))
+    lines = json.loads(lines)
+    #x = str(lines).encode("utf-8")
+#print(x)
+#print(str(lines['cards']).encode('utf-8'))
+#getlist of cards
+card_list = []
+
+for set_thing in lines.keys():
+    print(set_thing)
+
+    for key in lines[set_thing]['cards']:
+        #if "<class 'str'>" in key['name']:
+        #    continue
+        name =key['name'].encode('utf-8').decode('UTF-8')
+        card = db.cards_by_name[name]
+
+
+        if "Creature" == card.types[0]:
+            try:
+                print(card.name)
+                print(card.colors)
+                if len(card.colors) == 1:
+                    card_list.append(card)
+            except: 
+                pass
+
+        
+     
+
+#get the values we want from the list of cards
+for item in card_list:
+    try:
+        print("##",item.name)
+        if item.power == "*":
+            adjusted_power = "*"
+        elif item.power == "0":
+            adjusted_power = -1
+        elif "+" in item.power:
+            value = item.power.split("+")
+            adjusted_power = int(value[0]) / int(item.cmc)
+        
+        else:
+            adjusted_power =  int(item.power)/int(item.cmc) 
+
+        if item.toughness == "*":
+            adjusted_toughness = "*"
+        elif item.toughness == "0":
+            adjusted_toughness = -1
+        elif "+" in item.toughness:
+            value = item.toughness.split("+")
+            adjusted_toughness = int(value[0]) / int(item.cmc)
+        
+
+        else:
+            adjusted_toughness = int(item.toughness)/int(item.cmc)
+        
+        tf = ""
+        if len(item.subtypes) == 2:
+            tf = item.subtypes[1] 
+
+        ability = get_ability(item)  
+        
+        print(str(item.cmc) + "," + str(adjusted_power) + "," + str(adjusted_toughness) + "," + item.subtypes[0] + "," + tf  + "," + ability + "," + item.colors[0])
+    except:
+        pass
+
+
